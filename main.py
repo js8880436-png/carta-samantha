4552f145ed3968a1691d09ac71a99ccec35d8d70
js8880436-png <js8880436@gmail.com>
2026-08-10 18:38:23 -0300

    Add permission requests for external storage access
    Request permissions for reading and writing external storage.

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-android.permissions = INTERNET, READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE
+from android.permissions import request_permissions, Permission
+
+# Dentro do método build() da classe App, ou no início do código:
+request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
