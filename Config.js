47f36f61bd29cfe9daccbed94bbe0249a43d9e35
js8880436-png <js8880436@gmail.com>
2026-08-13 20:29:53 -0300

    Change SUPABASE_URL to use const declaration

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
-ferouvevxfzmdocg SUPABASE_URL = "https://ferouevevxfzmdocogjm.supabase.co";
+const SUPABASE_URL = "https://ferouevevxfzmdocogjm.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_NpSsFr5OE36ArCamSODpLA_qJ8mgR2o";
