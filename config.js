c87ba88c4ec52c5258c470f870dd137070cd576d
js8880436-png <js8880436@gmail.com>
2026-08-13 19:42:38 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Coloque aqui as credenciais públicas do seu projeto Supabase.
-window.SUPABASE_URL = "COLE_A_URL_DO_SUPABASE_AQUI";
-window.SUPABASE_ANON_KEY = "COLE_A_ANON_KEY_DO_SUPABASE_AQUI";
