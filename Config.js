d817e7fa2feccb559ffd512d18fab936093832a2
js8880436-png <js8880436@gmail.com>
2026-08-13 20:22:23 -0300

    Update comment for SUPABASE_ANON_KEY

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://ferouvevxfzmdocogjm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_NpSsFr..."; // cole a chave completa aqui
+const SUPABASE_ANON_KEY = "sb_publishable_NpSsFr..."; // cole a chave COMPLETA aqui
