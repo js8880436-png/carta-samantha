6b8668173e1f2418d98df8c7e89b8344e8e74fe7
js8880436-png <js8880436@gmail.com>
2026-08-13 20:40:31 -0300

    Move Supabase config to window object

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://ferouvevxfzmdocogjm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_NpSsFr5OE36ArCamSODpLA_qJ8mgR2o";
+window.SUPABASE_URL = "https://ferouvevxfzmdocogjm.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_NpSsFr5OE36ArCamSODpLA_qJ8mgR2o";
