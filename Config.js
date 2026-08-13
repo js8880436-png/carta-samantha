e45f5d3f13f338b2ccb88a9a9881eecd7e1c451b
js8880436-png <js8880436@gmail.com>
2026-08-13 20:34:17 -0300

    Fix Supabase URL formatting in Config.js

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://ferouevevxfzmdocogjm.supabase.co";
+const SUPABASE_URL = "https://ferouvevxfzmdocogjm.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_NpSsFr5OE36ArCamSODpLA_qJ8mgR2o";
